6f10c40963db61e37add70d7ae7e4653d5e2842a
Yaazieli <yaazieli@gmail.com>
2021-10-18 00:13:32 -0400

    commit tree 7df97a8fd9ab8226ae7a18af4203f076a5f2cbb4

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 2; 
+let num2 = 31;
+let multiply = num1 * num2;
+
+
+const random = Math.floor( Math.random() * 1000 ) + 1;
+
+let num3 = 22; 
+let num4 = 18;
+const mod = num3 % num4;
+
+
+const max = Math.max(5, 7, 10, 3, 20, 2);
